35075ef25d24f17bca22ee5c1ca9d501fb7111bb
Arthur L <arthurifccl@gmail.com>
2021-03-02 08:42:25 -0300

    -gta-

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,6 @@ case "deploy":
     new ResourceService();
     break; 
 default:
-    console.log("Use: five-cli\n\tnew-server - To make a new server config file\n\tnew-resource - To make a new resource config file\n\tstart - To start a new dev server based on config in directory\n\tdeploy - To deploy resource into dev server (the server need to be started before)");
+    console.log("Use: five-gta-cli\n\tnew-server - To make a new server config file\n\tnew-resource - To make a new resource config file\n\tstart - To start a new dev server based on config in directory\n\tdeploy - To deploy resource into dev server (the server need to be started before)");
     break;    
 }
